9f4dc1861f61c05ac37325e7fecca10394f82890
kelye <kelvinye2@gmail.com>
2023-09-10 17:00:57 +1000

    completed challenge

--- src/components/Advice/index.jsx
+++ src/components/Advice/index.jsx
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
-import Button from "../Button";
-import icon from "./icon-dice.svg"
+import icon from "./icon-dice.svg";
+import DesktopDivider from './pattern-divider-desktop.svg';
+import MobileDivider from './pattern-divider-mobile.svg';
+
 
 function Advice() {
   const [advice, setAdvice] = useState('');
+  const [number, setNumber] = useState('');
 
-  const url = "https://api.adviceslip.com/advice";
+  const url = "https://api.adviceslip.com/advice?t=" + Math.random();
 
   const fetchData = async () => {
     try {
@@ -13,6 +16,7 @@ function Advice() {
       const json = await response.json();
       console.log(json.slip.advice);
       setAdvice(json.slip.advice);
+      setNumber(json.slip.id);
     } catch (error) {
       console.log("error", error);
     }
@@ -24,7 +28,10 @@ function Advice() {
 
 
   return (<>
-    <div className="advice-text">{advice}</div>
+    <div className='advice-number'>ADVICE #{number}</div>
+    <div className="advice-text">"{advice}"</div>
+    <img className='divider-mobile' src={MobileDivider} />
+    <img className='divider-desktop' src={DesktopDivider} />
     <button className="reroll-button" onClick={fetchData} ><img className="button-icon" src={icon} /></button >
   </>
 
